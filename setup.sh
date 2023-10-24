8b3d8a429fe0deb390ac74913b6ae6c606f95321
Jack Stewart <jack.g.stewart@unsw.edu.au>
2023-10-25 01:37:57 +1100

    minor changes

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-ssh-keygen -f $SCRIPT_DIR/ot2_ssh_key
-curl -H 'Content-Type: application/json' -d "{\"key\":\"$(cat $SCRIPT_DIR/ot2_ssh_key.pub)\"}" $2:31950/server/ssh_keys
+ssh-keygen -f "${SCRIPT_DIR}/ot2_ssh_key"
+mv ot2_ssh_key ~
+chmod 600 ~/ot2_ssh_key
+# key_path=$(cat "${SCRIPT_DIR}/ot2_ssh_key.pub")
+echo $1
+curl -H 'Content-Type: application/json' -d "{\"key\":\"$(cat "${SCRIPT_DIR}/ot2_ssh_key.pub")\"}" $1:31950/server/ssh_keys
